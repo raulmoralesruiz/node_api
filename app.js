6b14da17467cc3027e528ba319e6adb4ae421530
Raúl Morales <raulmdaw@gmail.com>
2022-03-26 22:23:57 +0100

    Se agrega envío de logs a Slack

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dbConnect = require('./config/mongo')
+const dbConnect = require("./config/mongo");
+const morganBody = require("morgan-body");
+const loggerStream = require("./utils/handleLogger");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
 
+morganBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  },
+});
+
 const port = process.env.PORT || 3000;
 
 // invocar las rutas
-app.use("/api", require("./routes"))
+app.use("/api", require("./routes"));
 
 app.listen(port, () => {
-    console.log(`Servidor preparado. http://localhost:${port}`);
+  console.log(`Servidor preparado. http://localhost:${port}`);
 });
 
 dbConnect();
