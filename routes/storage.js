41418b5908cc7fa4aacd14d249a487a0d4404342
Raúl Morales <raulmdaw@gmail.com>
2022-03-27 21:12:06 +0200

    Se protegen las rutas de Storage

--- routes/storage.js
+++ routes/storage.js
@@ -5,6 +5,7 @@ const {
   createItem,
   deleteItem,
 } = require("../controllers/storage");
+const { authMiddleware } = require("../middleware/session");
 const uploadMiddleware = require("../utils/handleStorage");
 const router = express.Router();
 const { validatorGetItem } = require("../validators/storage");
@@ -12,21 +13,21 @@ const { validatorGetItem } = require("../validators/storage");
 /**
  * Lista todos los items
  */
-router.get("/", getItems);
+router.get("/", authMiddleware, getItems);
 
 /**
  * Obtener detalle de un item
  */
-router.get("/:id", validatorGetItem, getItem);
+router.get("/:id", authMiddleware, validatorGetItem, getItem);
 
 /**
  * Eliminar un item
  */
-router.delete("/:id", validatorGetItem, deleteItem);
+router.delete("/:id", authMiddleware, validatorGetItem, deleteItem);
 
 /**
  * Crea un item
  */
-router.post("/", uploadMiddleware.single("myfile"), createItem);
+router.post("/", authMiddleware, uploadMiddleware.single("myfile"), createItem);
 
 module.exports = router;
